cce346307624d31431e3589a2837ec54bfa9d2f2
Malachai Frazier <malachaifrazier@Malachais-MBP.lan>
2024-06-21 12:12:35 -0400

    git init for my fist gem

---
+++ spec/offensive_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Offensive do
+  it "has a version number" do
+    expect(Offensive::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
